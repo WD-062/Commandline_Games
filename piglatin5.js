be959c1c0c2c729d03f28f64075088a295947cdc
Sven Kaack <sven.kaack@gmx.de>
2025-04-10 00:22:47 +0200

    updated logic

--- piglatin5.js
+++ piglatin5.js
@@ -1,6 +1,10 @@
-const userText = process.argv.slice(2).toLowerCase();
-console.log(userText);
-let words = userText.split(" ");
+// const userText = process.argv.slice(2);
+// console.log(userText);
+// let words = userText.split(" ").toLowerCase();
+// console.log(words);
+
+const words = process.argv.slice(2);
+console.log(typeof words);
 console.log(words);
 
 const vowels = ["a", "e", "i", "o", "u"];
@@ -11,10 +15,10 @@ let firstLetter = words[i][0];
 let secondLetter = words[i][1];
 
 if (!firstLetter.includes(vowel) && secondLetter.includes(vowel)) {
-    let pigWord1 = words[i].slice(1) + words[i][0] + "ay";
+    let pigWord1 = words[i].slice(1) + words[i][0].toLowerCase() + "ay";
     console.log(pigWord1);
 } else if (!firstLetter.includes(vowel) && !secondLetter.includes(vowel)) {
-    let pigWord2 = words[i].slice(2) + words[i][(0, 1)] + "ay";
+    let pigWord2 = words[i].slice(2) + words[i][(0, 1)].toLowerCase() + "ay";
     console.log(pigWord2);
 } else if (firstLetter.includes(vowel)) {
     let pigWord3 = words[i] + "way";
